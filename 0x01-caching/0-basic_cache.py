d64f599a387010485c179deb83bbd02d5d7f649e
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-10-26 02:22:12 +0000

    added task one and a README.md file

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -12,16 +12,14 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         """
-        create a set key and item to create a dictionary
+        set a key and item to create a dictionary
         """
 
-        if key is not None or item is not None:
-            self.cache_data[key] = item
+        if key is not None and item is not None:
+            self.cache_data.update({key: item})
 
     def get(self, key):
         """
-        fetchs the value in  a dictionary using the key
+        fetches the value in  a dictionary using the key
         """
-        if key in self.cache_data:
-            return self.cache_data.get(key)
-        return None
+        return self.cache_data.get(key, None)
