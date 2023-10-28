00b10c0a3dde1dd2bf7c962d5234552983c0e603
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-10-28 03:48:16 +0000

    added task 3

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+3-lru_cache.py
+"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    implements the LRU cache algorithm
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        set a key to a value
+        """
+        if key is None and item is None:
+            return
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            list_keys = list(self.cache_data.keys())
+            least_used = list_keys[-1]
+            del self.cache_data[least_used]
+            print(f"Discard: {least_used}")
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        fetches a values in a dict with a given key
+        """
+
+        return self.cache_data.get(key, None)
