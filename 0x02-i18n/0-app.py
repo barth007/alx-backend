260754ea2a947af891c50b3ae095a94d81a18172
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-11-01 03:23:18 +0000

    started a flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template
+"""
+0-app.py
+"""
+app = Flask(__name__)
+
+
+@app.route("/index", strict_slashes=False)
+def index():
+    """
+    This renders an html page
+    """
+
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    """Entry point Main"""
+
+    app.run(host='0.0.0.0', port=5000, debug=True)
