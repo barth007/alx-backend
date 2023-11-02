f988afb55c08b843a17d42e6800846d1d81df7d3
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-11-02 20:43:32 +0000

    added task 2 to the project

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -30,7 +30,7 @@ def index():
     return render_template('2-index.html')
 
 
-# @babel.localeselector
+@babel.localeselector
 def get_locale():
     """
     selecting the prefer language from
@@ -40,7 +40,7 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-babel.init_app(app, locale_selector=get_locale)
+# babel.init_app(app, locale_selector=get_locale)
 
 
 if __name__ == "__main__":
