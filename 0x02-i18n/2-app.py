342abf08d244f99e47dd9d90336dedd5e0208586
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-11-02 16:58:10 +0000

    added task 2 to the project

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+1-app.py
+"""
+from flask_babel import Babel
+from flask import Flask, render_template
+
+
+class Config:
+    """
+    configuration of languages
+    """
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)   # loads the configuration settings
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    renders the html pages
+    """
+
+    return render_template('2-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """
+    selecting the prefer language from
+    the http request header by the user
+    """
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', 5000, debug=True)
