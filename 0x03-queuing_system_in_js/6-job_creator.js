e6b67ece1d73dce8ba24177b254dc90ae0e9eab8
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-12-03 06:02:43 +0000

    added task 7

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -2,8 +2,8 @@ import {createQueue} from 'kue';
 
 const push_notification_code = createQueue()
 var job = push_notification_code.create('myjob',{
-  phoneNumber: 'string',
-  message: 'string',
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account',
 }).save(err=>{
   if(!err){
     console.log('Notification job created:', job.id);
@@ -14,3 +14,13 @@ var job = push_notification_code.create('myjob',{
 .on('complete', ()=>{
   console.log('Notification job completed')
 })
+
+const sendNotification = (phoneNumber, message)=>{
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`)
+}
+
+push_notification_code.process('myjob', (job, done)=>{
+  const {phoneNumber, message} = job.data;
+  sendNotification(phoneNumber, message);
+  done();
+});
